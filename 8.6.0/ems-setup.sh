3de94ea027e5eebe3bcb23202e9ab2ec060cec45
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-09 13:06:33 +0200

    EMS setup for PSC 5.0.0-HF2 modified.

--- 8.6.0/ems-setup.sh
+++ 8.6.0/ems-setup.sh
@@ -3,8 +3,8 @@ docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/ems/ems-server:8.6.0
 cd "C:\Users\ALCH390\GolandProjects\TIBCO-EMS\8.6.0"
 docker-compose --file docker-compose-build-complete.yml build
 
-kubectl create ns ems
+kubectl create ns psc-ems
 cd "C:\Users\ALCH390\GolandProjects\TIBCO-EMS\8.6.0\helm\ems-service"
-helm install -n ems ems-service -f minikube-values.yaml .
+helm install -n psc-ems ems-service -f minikube-values.yaml .
 
 minikube tunnel
