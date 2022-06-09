aba59d5cbb2919e8b610f58a1c57e1a6bfa8d7fb
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-09 07:44:37 +0200

    Dockerfile and docker-compose-build-complete.yml modified for tibco-ems-client for PSC.

--- 8.6.0/ems-setup.sh
+++ 8.6.0/ems-setup.sh
@@ -1,6 +1,9 @@
-docker-compose --file docker-compose-build-complete.yml build
-kubectl create ns ems
+docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/ems/ems-server:8.6.0
 
+cd "C:\Users\ALCH390\GolandProjects\TIBCO-EMS\8.6.0"
+docker-compose --file docker-compose-build-complete.yml build
+
+kubectl create ns ems
 cd "C:\Users\ALCH390\GolandProjects\TIBCO-EMS\8.6.0\helm\ems-service"
 helm install -n ems ems-service -f minikube-values.yaml .
 
