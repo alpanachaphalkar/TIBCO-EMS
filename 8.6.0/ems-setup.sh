c32984ff7c0825c2ef916a430ef65004847eea86
alpanachaphalkar <alpana.chaphalkar@sky.de>
2022-06-23 12:22:43 +0200

    updated helm chart.

--- 8.6.0/ems-setup.sh
+++ 8.6.0/ems-setup.sh
@@ -1,3 +1,4 @@
+eval $(minikube docker-env) && docker pull eu.gcr.io/de-fo-gr-pr-shared/tibco/ems/ems-server:8.6.0 && docker pull busybox:latest
 kubectl create ns ems
 
 cd "C:\Users\ALCH390\GolandProjects\TIBCO-EMS\8.6.0\helm\ems-service"
